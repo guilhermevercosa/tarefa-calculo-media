d92a2902948b7a9926ef8d40007e5b016e7392c2
Guilherme Dias Verçosa <guilherme.vercosa20@gmail.com>
2024-12-22 21:34:39 -0300

    criação da atividade

---
+++ calculos.java
@@ -0,0 +1,23 @@
+public class calculos {
+    public static void main(String[] args) {
+        // **** Calculo de Média de Notas ****
+
+        // Declaração e inicialização das notas
+        int nota1 = 7;
+        int nota2 = 8;
+        int nota3 = 6;
+        int nota4 = 9;
+
+        // Cálculo da média
+        double media = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+        // Impressão das notas individuais
+        System.out.println("Nota 1: " + nota1);
+        System.out.println("Nota 2: " + nota2);
+        System.out.println("Nota 3: " + nota3);
+        System.out.println("Nota 4: " + nota4);
+
+        // Impressão da média
+        System.out.println("Média das notas: " + media);
+    }
+}
